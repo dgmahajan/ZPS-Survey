c95cf13c7268fc59c38bb92a1e13ce089af67ab1
dgmahajan <dgmahajan@gmail.com>
2026-04-29 11:38:00 +0530

    Add files via upload

--- surveys/index.js
+++ surveys/index.js
@@ -3,7 +3,7 @@
 //  Add each new survey filename here (without .js extension)
 // ============================================================
 
-const SUBMIT_URL = "https://script.google.com/macros/s/AKfycbxYr56VW_ZcnWJJrw0dgWlnMCVm_LZfUDa_lKLmwK56tUHtnum4TIXMFpMEofnDbkiGRQ/exec";
+const SUBMIT_URL = "https://script.google.com/macros/s/AKfycbwzbyF1LTX_yZzuoI0S1WBqTV0spVqFHRc7dzSJEpFyW8ndd2zMkoj_doLCp_r1cwB2aA/exec";
 
 const SURVEY_INDEX = [
   "murbad-solar-2026",
